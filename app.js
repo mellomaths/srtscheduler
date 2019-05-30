d8c8a71fba8ac8b643dc7496914995f41e5063b7
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2019-05-30 00:23:49 -0300

    inicio do controle de escalonamento

--- app.js
+++ app.js
@@ -20,9 +20,56 @@ document.querySelector('#process-form').addEventListener('submit', e => {
 
 document
     .querySelector('#listaDeProcessos')
-    .addEventListener('click', event => {
-        const id =
-            event.target.parentElement.parentElement.parentElement.id;
-        Interface.deleta(event.target);
+    .addEventListener('click', e => {
+        const id = e.target.parentElement.parentElement.parentElement.id;
+        Interface.deleta(e.target);
         scheduler.removeProcesso(id);
+    });
+
+document
+    .querySelector('#iniciarEscalonamento')
+    .addEventListener('click', e => {
+        const escalonamentoDiv = document.querySelector(
+            '#cardEscalonamento'
+        );
+        for (let i = 0; i < scheduler.processos.length; i++) {
+            const processo = scheduler.processos[i];
+            const div = document.createElement('div');
+            div.classList.add('mt-2');
+            div.innerHTML = `
+                <label for="progresso${processo.id}">Processo ${
+                processo.id
+            }</label>
+                <div class="progress" id="progresso${processo.id}">
+                    <div
+                        id="progresso${processo.id}-nonexisting"
+                        class="progress-bar progress-bar-striped progress-bar-animated"
+                        role="progressbar"
+                        style="width: 15%"
+                        aria-valuenow="15"
+                        aria-valuemin="0"
+                        aria-valuemax="100"
+                    ></div>
+                    <div
+                        id="progresso${processo.id}-executing"
+                        class="progress-bar progress-bar-striped progress-bar-animated bg-success"
+                        role="progressbar"
+                        style="width: 30%"
+                        aria-valuenow="30"
+                        aria-valuemin="0"
+                        aria-valuemax="100"
+                    ></div>
+                    <div
+                        id="progresso${processo.id}-stopped"
+                        class="progress-bar progress-bar-striped progress-bar-animated bg-danger"
+                        role="progressbar"
+                        style="width: 20%"
+                        aria-valuenow="20"
+                        aria-valuemin="0"
+                        aria-valuemax="100"
+                    ></div>
+                </div>
+            `;
+            escalonamentoDiv.appendChild(div);
+        }
     });
